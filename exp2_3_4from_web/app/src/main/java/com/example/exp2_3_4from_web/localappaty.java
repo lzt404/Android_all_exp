65d05cc4341cab6e4109087f6db4e1156959f8c5
lzt404 <2596933790@qq.com>
2025-03-07 00:28:06 +0800

    finish 2.3.4

---
+++ exp2_3_4from_web/app/src/main/java/com/example/exp2_3_4from_web/localappaty.java
@@ -0,0 +1,34 @@
+package com.example.exp2_3_4from_web;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class localappaty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_localappaty);
+        Uri uri = getIntent().getData();
+        TextView txt = (TextView) findViewById(R.id.tv2);
+        String suri = Uri.decode(uri.toString());
+        String data = uri.getQueryParameter("data");
+        txt.setText("Uri 和查询参数suri:" + suri + "查询参数data:" + data );
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
