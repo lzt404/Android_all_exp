93504863347db7711954abff08b1a0e1a9c0fc57
lzt404 <2596933790@qq.com>
2025-03-07 08:53:51 +0800

    finish 2.4.1aty_message

---
+++ exp2_4_1aty_message/app/src/main/java/com/example/exp2_4_1aty_message/recv.java
@@ -0,0 +1,31 @@
+package com.example.exp2_4_1aty_message;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class recv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recv);
+        Intent intent = getIntent();
+        TextView tv = (TextView) findViewById(R.id.textView);
+        String name = intent.getStringExtra("name");
+        int age = intent.getIntExtra("age", 0);
+        tv.setText("收到:" + name + age);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
