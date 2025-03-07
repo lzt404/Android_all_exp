a11b9a6b47e5db838f2c14f0bf41bd11ad2c9e1e
lzt404 <2596933790@qq.com>
2025-03-07 15:04:37 +0800

    finish 2.4.3

---
+++ exp2_4_3sendObject/app/src/main/java/com/example/exp2_4_3sendobject/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.example.exp2_4_3sendobject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        User user = (User) intent.getSerializableExtra("user");
+        tv.setText(user.getName()  + user.getAge());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ exp2_4_3sendObject/app/src/main/java/com/example/exp2_4_3sendobject/User.java
@@ -0,0 +1,20 @@
+package com.example.exp2_4_3sendobject;
+
+import java.io.Serializable;
+
+public class  User implements Serializable {
+    private String name;
+    private int age;
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public  String getName() {return  name;}
+    public  int     getAge() {return  age;}
+
+    public  void setName(String name) {this.name = name;}
+
+    public  void setAge(int age) {this.age = age;}
+
+}
