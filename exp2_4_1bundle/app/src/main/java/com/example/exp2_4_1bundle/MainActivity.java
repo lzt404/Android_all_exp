3ff362e89e9d9eddfcdf3ab5277d5511ff47b6cf
lzt404 <2596933790@qq.com>
2025-03-07 09:15:13 +0800

    finish 2.4.1bundle

---
+++ exp2_4_1bundle/app/src/main/java/com/example/exp2_4_1bundle/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.exp2_4_1bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, recv.class);
+                Bundle bd = new Bundle();
+                bd.putString("name", "梁展通");
+                bd.putInt("age", 20);
+                intent.putExtras(bd);
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ exp2_4_1bundle/app/src/main/java/com/example/exp2_4_1bundle/recv.java
@@ -0,0 +1,32 @@
+package com.example.exp2_4_1bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class recv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recv);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        Intent itent = getIntent();
+        Bundle bd = itent.getExtras();
+        String name = bd.getString("name");
+        int age = bd.getInt("age");
+        tv.setText("收到的数据" + name + age);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
